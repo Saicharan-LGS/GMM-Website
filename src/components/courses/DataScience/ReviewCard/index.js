900e9b646760b5f374a7edcb7ae9065933d3b6f0
Kapilrajreddy <kapilrajreddy@gmail.com>
2023-08-08 15:53:53 +0530

    review

--- src/components/courses/DataScience/ReviewCard/index.js
+++ src/components/courses/DataScience/ReviewCard/index.js
@@ -3,11 +3,11 @@ import "./index.css"
 class ReviewCard extends Component{
     render(){
         const {reviewDetails}=this.props
-        const {name,review,designation,time}=reviewDetails
+        const {image,name,review,designation,time}=reviewDetails
         return(
             <div className="courseReviewCard">
                 <div className="courseReviewFirstContainer">
-                    <img className="courseReviewProfileImage" alt="profile" src="https://marketplace.canva.com/EAFXS8-cvyQ/1/0/1600w/canva-brown-and-light-brown%2C-circle-framed-instagram-profile-picture-2PE9qJLmPac.jpg" />
+                    <img className="courseReviewProfileImage" alt="profile" src={image} />
                     <div>
                         <p className="courseReviewName">{name}</p>
                         <p className="courseReviewDesignation">{designation}</p>
