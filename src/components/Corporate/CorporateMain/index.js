fb69b39a99afbc2b8e0d50e3707d75963d1d1b05
Arun Kumar Chelikani <chelikani119@gmail.com>
2023-08-31 17:00:40 +0530

    #added content

--- src/components/Corporate/CorporateMain/index.js
+++ src/components/Corporate/CorporateMain/index.js
@@ -37,45 +37,45 @@ const ourPatners=[
 
 const consultativeCardData = [
     {
-      title: 'Aligning Learning Outcomes with Business Goals',
-      listHead: 'Our Sales team will:',
+      title: 'Aligning Achievements in Learning with Corporate Objectives',
+      listHead: 'Our Sales team is committed to aligning with your business:',
       listItems: [
-        'Understand your business goals',
-        'Recognize target audience and current challenges',
-        'Propose the best learning solution for your business',
+        'Grasp a deep understanding of your business goals.',
+        'Identify your target audience and prevailing challenges.',
+        'Suggest the most effective learning solution tailored to your business needs.',
       ],
       icon: <BsPuzzle size={40} color="#ffffff" />,
       backgroundColor: '#651cd5',
     },
     {
-        title: 'Aligning Learning Outcomes with Business Goals',
-        listHead: 'Our Sales team will:',
+        title: 'Tailoring a Unique Learning Pathway',
+        listHead: 'Our Solutioning & SME team is here to:',
         listItems: [
-          'Understand your business goals',
-          'Recognize target audience and current challenges',
-          'Propose the best learning solution for your business',
+          'Evaluate your requirements through our exclusive Skills Framework',
+          'Craft a personalized learning journey in consultation with your stakeholders',
+          'Work closely with you to establish clear and agreed-upon learning objectives.',
         ],
         icon: <CgNotes size={40} color="#ffffff"  />,
         backgroundColor: '#f1644c',
       },
       {
-        title: 'Aligning Learning Outcomes with Business Goals',
-        listHead: 'Our Sales team will:',
+        title: 'Boosting Learner Engagement',
+        listHead: 'Our Program Delivery Team is dedicated to:',
         listItems: [
-          'Understand your business goals',
-          'Recognize target audience and current challenges',
-          'Propose the best learning solution for your business',
+          'Guaranteeing active learner involvement and program excellence',
+          'Maintaining an ongoing feedback loop to enhance learner contentment',
+          'Offering around-the-clock support for a frictionless experience.',
         ],
         icon: <BsFileBarGraph size={40} color="#ffffff"  />,
         backgroundColor: '#ffcc00',
       },
       {
-        title: 'Aligning Learning Outcomes with Business Goals',
-        listHead: 'Our Sales team will:',
+        title: 'Securing Program Excellence via Governance',
+        listHead: 'Our Governance Team assists you in:',
         listItems: [
-          'Understand your business goals',
-          'Recognize target audience and current challenges',
-          'Propose the best learning solution for your business',
+          'Gaining insights into learner progress for outcome assessment',
+          'Implementing adaptive adjustments based on feedback for continual improvement',
+          'Delivering regular progress reports to guarantee the triumph of your program.',
         ],
         icon: <LuAlarmClock size={40} color="#ffffff"  />,
         backgroundColor: '#0084ff',
@@ -187,8 +187,8 @@ export default function Corporate() {
             <Header />
             <div className='enterprise-banner-section'>
                 <div className='enterprise-banner-section-conatiner'>
-                    <h3 className='enterprise-banner-section-title'>Delivering Learning Outcomes for Businesses</h3>
-                    <p className='enterprise-banner-section-subtitle'>We are a lifelong learning partner for corporates, helping them build skills in emerging technologies at scale</p>
+                    <h3 className='enterprise-banner-section-title'>Achieving Educational Objectives for Enterprises.</h3>
+                    <p className='enterprise-banner-section-subtitle'>We serve as a continuous learning ally for corporations, assisting them in developing scalable expertise in cutting-edge technologies.</p>
                     <p className='enterprise-banner-section-change-text'>{bannerRender[currentIndex]}</p>
                     
                         <a href="https://api.whatsapp.com/send?phone=919866238940" rel="noreferrer" target="_blank"><button className='enterprise-banner-section-button' >Learn More</button></a>
@@ -204,13 +204,13 @@ export default function Corporate() {
                     <img className='enterprise-industry-section-image' src="https://d1vwxdpzbgdqj.cloudfront.net/assets/enterprise-new/iso-logo-1f29cec08343f009564ec969b6e154b1d1175213c214de1f97c02859b055dea8.jpg" alt="" />
                     <div className='enterprise-industry-section-certificate-conatiner-content'>
                         <p className='enterprise-industry-section-certificate-heading'>Glorious Mindmine is ISO 27001 Certified</p>
-                        <p className='enterprise-industry-section-certificate-content'>ISO/IEC 27001 is the world's best-known standard for information security management systems (ISMS) and their requirements. We at Great Learning have the industry best Data Privacy & Security practices, and the ISO 27001 certification from ISOQAR of UK is a testament of the same.</p>
+                        <p className='enterprise-industry-section-certificate-content'>ISO/IEC 27001 is the world's best-known standard for information security management systems (ISMS) and their requirements. We at GloriousMindMine have the industry best Data Privacy & Security practices, and the ISO 27001 certification from ISOQAR of UK is a testament of the same.</p>
                     </div>
                 </div>
             </div>
             <div className='enterprise-partners-section-conatiner'>
-                <p className='enterprise-partners-section-conatiner-subtitle'>Helping Our Partners</p>
-                <h1 className='enterprise-partners-section-title'>Solutions To Empower Corporates</h1>
+                <p className='enterprise-partners-section-conatiner-subtitle'>Supporting Our Collaborators</p>
+                <h1 className='enterprise-partners-section-title'>Corporate Empowerment Solutions.</h1>
                 <div className='enterprise-partners-section-cards-conatiner'>
                     {ourPatners.map((partner, index) => (
                             <div key={index} className='enterprise-partners-section-card-conatiner'> 
@@ -224,8 +224,8 @@ export default function Corporate() {
                 </div>
             </div>
             <div className='enterprise-consultative-section-conatiner'>
-                <p className='enterprise-partners-section-conatiner-subtitle'>Consultative Methodology</p>
-                <h1 className='enterprise-partners-section-title'>How We Help Businesses Grow</h1>
+                <p className='enterprise-partners-section-conatiner-subtitle'>Advisory Approach</p>
+                <h1 className='enterprise-partners-section-title'>Enabling Business Expansion</h1>
                 <div className='enterprise-consultative-section-cards-container'>
                     {consultativeCardData.map((card, index) => (
                     <div key={index} className='enterprise-consultative-section-card-container'>
@@ -264,7 +264,7 @@ export default function Corporate() {
                 <div className='enterprise-business-section-container-content'>
                     <p className='enterprise-business-section-subtitle'>500+ corporates use </p>
                     <h1 className='enterprise-business-section-title'>Glorious for Business</h1>
-                    <p className='enterprise-business-section-pargargh'>Get in touch with our team to discuss how Great Learning for Business can power your Enterprise Learning.</p>
+                    <p className='enterprise-business-section-pargargh'>Get in touch with our team to discuss how GloriousMindMine for Business can power your Enterprise Learning.</p>
                     <div>
                     <a href="https://api.whatsapp.com/send?phone=919866238940" rel="noreferrer" target="_blank"><button className='enterprise-partners-section-button'>TALK TO US</button></a>
                     </div>
--- src/components/Corporate/EnterpriceBegineer/index.js
+++ src/components/Corporate/EnterpriceBegineer/index.js
@@ -13,43 +13,43 @@ import {HiOutlineUsers} from 'react-icons/hi'
 import {PiSimCard} from 'react-icons/pi'
 import {GrFormCheckmark} from 'react-icons/gr'
 const Enterprisebeginner = [
-    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Drive Culture Of Learning",enterpriseDesc:"Video content of 1,000+ hours across 700+ courses in emerging technologies",color:"#651CD5"},
-    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Empower Your Workforce",enterpriseDesc:"80+ curated career path-based courses to help employees upskill.",color:"#F1644C"},
-    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Measure Organization Learning Health",enterpriseDesc:"Actionable insights via dashboards & learner metrics to identify HIPOs and key successors.",color:"#FFCC00"},
-    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Learn From Industry Experts",enterpriseDesc:"Access to webinars & masterclasses with Industry leaders.",color:"#0084FF"},
-    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Courses Designed by Experts",enterpriseDesc:"Content created by world class Ivy faculty & industry experts.",color:"#D64B9E"},
-    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Personalized Learning Experience",enterpriseDesc:"Co-branded LMS & certificates to reward employees with R&R Policy.",color:"#2FA75F"}
+    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Learning-Centric Culture",enterpriseDesc:"Over 700 courses in emerging technologies, offering more than 1,000 hours of video content.",color:"#651CD5"},
+    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Strengthen Your Workforce",enterpriseDesc:"Over 80 meticulously curated courses designed to assist employees in advancing their skills based on career paths.",color:"#F1644C"},
+    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Evaluate Organizational Learning Well-being",enterpriseDesc:"Obtain practical insights using dashboards and learner metrics to pinpoint High-Potential (HIPO) individuals and critical successors.",color:"#FFCC00"},
+    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Gain Knowledge from Seasoned Industry Professionals",enterpriseDesc:"Enjoy Webinars and Masterclasses Featuring Industry Pioneers.",color:"#0084FF"},
+    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Expert-Crafted Courses",enterpriseDesc:"Materials developed by distinguished Ivy League faculty and industry authorities.",color:"#D64B9E"},
+    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Tailored Learning Journey",enterpriseDesc:"Customized Learning Management System (LMS) and Certificates to Recognize and Reward Employees in Line with Our R&R Policy.",color:"#2FA75F"}
 
 ]
 
 const EnterpriseGraduate = [
-    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Culture Of Learning",enterpriseDesc:"Video content of 1,000+ hours across 700+ courses in emerging technologies",color:"#651CD5"},
-    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Empower Your Workforce",enterpriseDesc:"80+ curated career path-based courses to help employees upskill.",color:"#F1644C"},
-    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Measure Organization Learning Health",enterpriseDesc:"Actionable insights via dashboards & learner metrics to identify HIPOs and key successors.",color:"#FFCC00"},
-    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Learn From Industry Experts",enterpriseDesc:"Access to webinars & masterclasses with Industry leaders.",color:"#0084FF"},
-    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Courses Designed by Experts",enterpriseDesc:"Content created by world class Ivy faculty & industry experts.",color:"#D64B9E"},
-    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Personalized Learning Experience",enterpriseDesc:"Co-branded LMS & certificates to reward employees with R&R Policy.",color:"#2FA75F"}
+    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Pre-Commencement Training Program",enterpriseDesc:"Initiate training for new hires prior to their onboarding, featuring a comprehensive 12-week curriculum.",color:"#651CD5"},
+    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Enhanced Operational Efficiency",enterpriseDesc:"Trust the professionals and enable your employees to concentrate on the core aspects of your business.",color:"#F1644C"},
+    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Cost-Efficient Results",enterpriseDesc:"Achieve a minimum savings of 75% compared to your current campus hiring and training model.",color:"#FFCC00"},
+    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Explore a Selection of 6+ Domains",enterpriseDesc:"Including Data Science, AI, Machine Learning, Full Stack Development, Cybersecurity, and Cloud Computing.",color:"#0084FF"},
+    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Attain a 100% Completion Rate",enterpriseDesc:"Ensure your learners are trained according to your business schedule without any delays.",color:"#D64B9E"},
+    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Assess Freshmen Engagement",enterpriseDesc:"Recognize the percentage of disengaged learners early on to facilitate resource allocation.",color:"#2FA75F"}
 
 ]
 
 
 const EnterpriseEmployees = [
-    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Culture Of Learning",enterpriseDesc:"Video content of 1,000+ hours across 700+ courses in emerging technologies",color:"#651CD5"},
-    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Empower Your Workforce",enterpriseDesc:"80+ curated career path-based courses to help employees upskill.",color:"#F1644C"},
-    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Measure Organization Learning Health",enterpriseDesc:"Actionable insights via dashboards & learner metrics to identify HIPOs and key successors.",color:"#FFCC00"},
-    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Learn From Industry Experts",enterpriseDesc:"Access to webinars & masterclasses with Industry leaders.",color:"#0084FF"},
-    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Courses Designed by Experts",enterpriseDesc:"Content created by world class Ivy faculty & industry experts.",color:"#D64B9E"},
+    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Over 35 Job-Role Specific Certifications",enterpriseDesc:"Offering programs across 7+ domains including AI/ML, Cloud Computing, Data Science, Cybersecurity, and Full Stack Development.",color:"#651CD5"},
+    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Bespoke Short Programs",enterpriseDesc:"Obtain tailored learning journeys spanning 3-6 months, aligned with your business needs.",color:"#F1644C"},
+    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Reduce Attrition and Enhance Staff Retention",enterpriseDesc:"Leverage your existing workforce to lower recruitment expenditures and expedite the achievement of business objectives.",color:"#FFCC00"},
+    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Harness Emerging Technologies",enterpriseDesc:"Elevate your workforce's proficiency in cutting-edge technologies and gain a competitive edge.",color:"#0084FF"},
+    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Certification from Prestigious Global Academies",enterpriseDesc:"Our academic team comprises over 3,000 Ivy League faculty and industry experts.",color:"#D64B9E"},
     {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Personalized Learning Experience",enterpriseDesc:"Co-branded LMS & certificates to reward employees with R&R Policy.",color:"#2FA75F"}
 
 ]
 
 const EnterpriseContinuousLearners = [
-    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Culture Of Learning",enterpriseDesc:"Video content of 1,000+ hours across 700+ courses in emerging technologies",color:"#651CD5"},
-    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Empower Your Workforce",enterpriseDesc:"80+ curated career path-based courses to help employees upskill.",color:"#F1644C"},
-    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Measure Organization Learning Health",enterpriseDesc:"Actionable insights via dashboards & learner metrics to identify HIPOs and key successors.",color:"#FFCC00"},
-    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Learn From Industry Experts",enterpriseDesc:"Access to webinars & masterclasses with Industry leaders.",color:"#0084FF"},
-    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Courses Designed by Experts",enterpriseDesc:"Content created by world class Ivy faculty & industry experts.",color:"#D64B9E"},
-    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Personalized Learning Experience",enterpriseDesc:"Co-branded LMS & certificates to reward employees with R&R Policy.",color:"#2FA75F"}
+    {id:1,EnterpriseIcon:LiaBullhornSolid,enterpriseHeading:"Global Programs",enterpriseDesc:"Offer Your Team a Range of Options with 35+ Programs from Global Academies",color:"#651CD5"},
+    {id:2,EnterpriseIcon:MdWorkOutline,enterpriseHeading:"Reward High-Achieving Talent",enterpriseDesc:"Support employees who contribute significantly to your business outcomes by sponsoring their participation in these programs.",color:"#F1644C"},
+    {id:3,EnterpriseIcon:MdOutlineHealthAndSafety,enterpriseHeading:"Provide Growth Opportunities",enterpriseDesc:"Empower employees to enroll in premium programs and receive reimbursements as part of our Education Policy.",color:"#FFCC00"},
+    {id:4,EnterpriseIcon:HiOutlineUsers,enterpriseHeading:"Facilitate Skill Advancement",enterpriseDesc:"Explore internal career progression opportunities to remain current and valuable within the industry.",color:"#0084FF"},
+    {id:5,EnterpriseIcon:PiSimCard,enterpriseHeading:"Track Learner Progress",enterpriseDesc:"Provide insights to both the organization and the learner for enhanced learning patterns.",color:"#D64B9E"},
+    {id:6,EnterpriseIcon:GrFormCheckmark,enterpriseHeading:"Committed Program Assistance",enterpriseDesc:"Provide dedicated support to employees to encourage high engagement and completion rates.",color:"#2FA75F"}
 
 ]
 
@@ -93,7 +93,7 @@ function EnterpriseBeginner(){
             <div>
                 <div className="Enterprise-card-main-text-container">
                     <p className="Enterprise-card-beginner-main-heading">For Beginners</p>
-                    <h2 className="Enterprise-card-beginner-sub-heading">Foundation Skill Library</h2>
+                    <h2 className="Enterprise-card-beginner-sub-heading">Fundamental Skill Resource</h2>
                 </div>
                 <div className="Enterprise-beginner-slider-container">
                         <Slider {...Enterprisesettings} className="Enterprise-beginner-slider">
@@ -119,20 +119,20 @@ function EnterpriseBeginner(){
             </div>
             <div className="Enterprise-review-main-container">
                 <div className="Enterprise-review-of-recruiters">
-                    <p className="Enterprise-review-of-recruiters-heading">Growth Story: Campus Recruiters</p>
-                    <p className="Enterprise-review-of-recruiters-description">We helped 400+ freshers get trained in data science which eventually helped the business gain 2x in revenue and cut HR costs by INR 1.7 billion.</p>
+                    <p className="Enterprise-review-of-recruiters-heading">Success in Campus Recruitment: Our Impact</p>
+                    <p className="Enterprise-review-of-recruiters-description">We've supported over 400 recent graduates in mastering data science, leading to a twofold increase in revenue and a reduction of INR 1.7 billion in HR expenses for our business.</p>
                     <p className="Enterprise-review-of-recruiters-case-study">READ FULL CASE STUDY</p>
                 </div>
                 <div className="Enterprise-review-of-partner">
-                    <p className="Enterprise-review-of-partner-heading">We highly recommend Great Learning for Business as a learning partner.</p>
-                    <p className="Enterprise-review-of-partner-description">“Their attention to detail, innovative ideas, rigorous curricula, and commitment to delivering the highest quality of learning programs have been very impressive.”</p>
+                    <p className="Enterprise-review-of-partner-heading">We strongly endorse GloriousMindMine for Business as a learning collaborator.</p>
+                    <p className="Enterprise-review-of-partner-description">“Their meticulous approach, inventive concepts, comprehensive curricula, and unwavering dedication to delivering top-notch learning initiatives have left a remarkable impression.”</p>
                     <p className="Enterprise-review-of-partner-analytics">VP,Analytics</p>
                 </div>
             </div>
             <div>
                 <div className="Enterprise-card-main-text-container">
                     <p className="Enterprise-card-beginner-main-heading">For Mid-Senior Level Employees</p>
-                    <h2 className="Enterprise-card-beginner-sub-heading">Digital Skills Certificate</h2>
+                    <h2 className="Enterprise-card-beginner-sub-heading">Certificate in Digital Skills</h2>
                 </div>
                 <div className="Enterprise-beginner-slider-container">
                         <Slider {...Enterprisesettings} className="Enterprise-beginner-slider">
@@ -142,20 +142,20 @@ function EnterpriseBeginner(){
             </div>
             <div className="Enterprise-review-main-container">
                 <div className="Enterprise-review-of-recruiters">
-                    <p className="Enterprise-review-of-recruiters-heading">Growth Story: Campus Recruiters</p>
-                    <p className="Enterprise-review-of-recruiters-description">We helped 400+ freshers get trained in data science which eventually helped the business gain 2x in revenue and cut HR costs by INR 1.7 billion.</p>
-                    <p className="Enterprise-review-of-recruiters-case-study">READ FULL CASE STUDY</p>
+                    <p className="Enterprise-review-of-recruiters-heading">Success Story: Mid-Senior Level Employees</p>
+                    <p className="Enterprise-review-of-recruiters-description">We revolutionized the Data Analytics & Modeling workforce, resulting in a threefold increase in revenue and a threefold boost in productivity for the business.</p>
+                    <p className="Enterprise-review-of-recruiters-case-study">Read the Complete Case Study</p>
                 </div>
                 <div className="Enterprise-review-of-partner">
-                    <p className="Enterprise-review-of-partner-heading">We highly recommend Great Learning for Business as a learning partner.</p>
-                    <p className="Enterprise-review-of-partner-description">“Their attention to detail, innovative ideas, rigorous curriculam, and commitment to delivering the highest quality of learning programs have been very impressive.”</p>
+                    <p className="Enterprise-review-of-partner-heading">GloriousMindMine for Business has been a valued learning collaborator for several years.</p>
+                    <p className="Enterprise-review-of-partner-description">“Their meticulous approach, creative thinking, rigorous course content, and dedication to providing top-quality customized learning programs have left a strong impression. The mentor-led sessions consistently garner excellent reviews and feedback.”</p>
                     <p className="Enterprise-review-of-partner-analytics">VP,Analytics</p>
                 </div>
             </div>
             <div>
                 <div className="Enterprise-card-main-text-container">
                     <p className="Enterprise-card-beginner-main-heading">For Continuous Learners</p>
-                    <h2 className="Enterprise-card-beginner-sub-heading">Premium Executive Programs</h2>
+                    <h2 className="Enterprise-card-beginner-sub-heading">Exclusive Executive Programs</h2>
                 </div>
                 <div className="Enterprise-beginner-slider-container">
                     <Slider {...Enterprisesettings} className="Enterprise-beginner-slider">
